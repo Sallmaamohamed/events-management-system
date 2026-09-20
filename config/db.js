0b991e153a51b46bb480b9e8a46103b25a942079
Sallmaamohamed <salmamohamed77127@gmail.com>
2026-09-20 15:55:10 +0300

    update backend

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log('Database connected: ${conn.connection.host}');
+        console.log('Connecting to MongoDB...');
+
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        });
+
+        console.log(`Database connected: ${conn.connection.host}`);
     } catch (err) {
-        console.error('connection erroe: ${err.message}');
+        console.error(`Connection error: ${err.message}`);
         process.exit(1);
     }
 };
+
 module.exports = connectDB;
