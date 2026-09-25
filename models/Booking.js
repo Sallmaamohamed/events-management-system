9ee19046cb7ff7d0aa7b84d96e66e4fc98855a05
maromoabark <maromobarak205@gmail.com>
2026-09-26 02:17:28 +0300

    Create Booking model with schema for bookings

---
+++ models/Booking.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+      required: true,
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Booking', bookingSchema);
